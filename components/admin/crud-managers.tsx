3c955633d431069a886897a5252319d09516477f
Developer <mebratelias@gmail.com>
2026-09-07 10:19:20 +0300

    chore: clean unused imports

--- components/admin/crud-managers.tsx
+++ components/admin/crud-managers.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useTransition } from "react";
 import Image from "next/image";
-import { Pencil, Trash2, Plus } from "lucide-react";
+import { Pencil, Trash2 } from "lucide-react";
 import { toast } from "sonner";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
